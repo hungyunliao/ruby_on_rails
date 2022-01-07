254479edcf5e143b1fb758413da203f63cc083af
Liao <hungyunl@88665a279eef.ant.amazon.com>
2022-01-07 09:38:42 -0800

    Change indentation from 4 to 2.

--- ror/app/controllers/concerns/json_response.rb
+++ ror/app/controllers/concerns/json_response.rb
@@ -1,9 +1,9 @@
 module JsonResponse
-    extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-    RESPONSE_STATUS = {
-        :SUCCESS => "success",
-        :ERROR => "error"
-    }
+  RESPONSE_STATUS = {
+    :SUCCESS => "success",
+    :ERROR => "error"
+  }
 
 end
--- ror/app/controllers/tags_controller.rb
+++ ror/app/controllers/tags_controller.rb
@@ -1,134 +1,134 @@
 class TagsController < ApplicationController
-    include JsonResponse
+  include JsonResponse
 
-    def index
-        @filter = params["filter"]
-        @tags = @filter? Tag.where("name LIKE ?", "%#{@filter}%") : Tag.all
-        render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS],
-                                    @tags)
+  def index
+    @filter = params["filter"]
+    @tags = @filter? Tag.where("name LIKE ?", "%#{@filter}%") : Tag.all
+    render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS],
+                  @tags)
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS],
+                    @tag)
+    else
+      render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
+                    @tag.errors)
     end
+  end
 
-    def create
-        @tag = Tag.new(tag_params)
-        if @tag.save
-            render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS],
-                                        @tag)
-        else
-            render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
-                                        @tag.errors)
-        end
-    end
-
-    def update
-        # find_by() returns nil if nothing found. find() raises an exception.
-        @tag = Tag.find_by(id: params[:id])
-        if !@tag
-            render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
-                                        "Tag not found.")
-            return
-        end 
-        
-        if @tag.update(tag_params)
-            render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS],
-                                        @tag)
-        else
-            render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
-                                        "Fail to update the tag.")
-        end
-    end
-
-    def destroy
-        @tag = Tag.find_by(id: params[:id])
-        if !@tag
-            render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
-                                        "Tag not found.")
-            return
-        end 
-
-        if @tag.taggings_count == 0
-            @tag.destroy
-            render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS])
-        else
-            render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
-                                        "Cannot delete the tag.")
-        end
-    end
+  def update
+    # find_by() returns nil if nothing found. find() raises an exception.
+    @tag = Tag.find_by(id: params[:id])
+    if !@tag
+      render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
+                    "Tag not found.")
+      return
+    end 
     
-    #
-    # Nested endpoints
-    #
-    def article_tags
-        @article = Article.find_by(id: params[:article_id])
-        if !@article
-            render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
-                                        "Article not found.")
-            return
-        end 
+    if @tag.update(tag_params)
+      render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS],
+                    @tag)
+    else
+      render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
+                    "Fail to update the tag.")
+    end
+  end
 
-        render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS],
-                                    @article.tags)
+  def destroy
+    @tag = Tag.find_by(id: params[:id])
+    if !@tag
+      render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
+                    "Tag not found.")
+      return
+    end 
+
+    if @tag.taggings_count == 0
+      @tag.destroy
+      render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS])
+    else
+      render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
+                    "Cannot delete the tag.")
+    end
+  end
+  
+  #
+  # Nested endpoints
+  #
+  def article_tags
+    @article = Article.find_by(id: params[:article_id])
+    if !@article
+      render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
+                    "Article not found.")
+      return
+    end 
+
+    render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS],
+                  @article.tags)
+  end
+
+  def attach_tag
+    @article = Article.find_by(id: params[:article_id])
+    @tag = Tag.find(params[:tag_id])
+    if !@article || !@tag
+      render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
+                    "Article or Tag not found.")
+      return
+    end 
+
+    begin
+      @article.tags << @tag
+      render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS],
+                    @article.tags)
+    rescue ActiveRecord::RecordInvalid => error
+      render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
+                    "Tag already attached.")
+    end
+  end
+
+  def detach_tag
+    @article = Article.find_by(id: params[:article_id])
+    @tag = Tag.find_by(id: params[:id])      
+    if !@article || !@tag
+      render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
+                    "Article or Tag not found.")
+      return
+    end 
+
+    @target_tag_id = params[:id].to_i   # Convert params[:id] from String to Integer
+    @article.tags.each do |tag|
+      if tag.id == @target_tag_id
+        @article.tags.delete(tag)
+      end
+    end
+    render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS],
+                  @article.tags)
+  end
+
+  def retreive_articles
+    @tag = Tag.find_by(id: params[:tag_id])
+    if !@tag
+      render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
+                    "Tag not found.")
+      return
+    end 
+
+    @articles = @tag.articles
+    render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS],
+                  @articles)
+  end
+
+  private
+    def tag_params
+      params.permit(:name)
     end
 
-    def attach_tag
-        @article = Article.find_by(id: params[:article_id])
-        @tag = Tag.find(params[:tag_id])
-        if !@article || !@tag
-            render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
-                                        "Article or Tag not found.")
-            return
-        end 
-
-        begin
-            @article.tags << @tag
-            render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS],
-                                        @article.tags)
-        rescue ActiveRecord::RecordInvalid => error
-            render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
-                                        "Tag already attached.")
-        end
+    def json_response(status, message="")
+      {
+        "status" => status,
+        "message" => message
+      }
     end
-
-    def detach_tag
-        @article = Article.find_by(id: params[:article_id])
-        @tag = Tag.find_by(id: params[:id])      
-        if !@article || !@tag
-            render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
-                                        "Article or Tag not found.")
-            return
-        end 
-
-        @target_tag_id = params[:id].to_i   # Convert params[:id] from String to Integer
-        @article.tags.each do |tag|
-            if tag.id == @target_tag_id
-                @article.tags.delete(tag)
-            end
-        end
-        render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS],
-                                    @article.tags)
-    end
-
-    def retreive_articles
-        @tag = Tag.find_by(id: params[:tag_id])
-        if !@tag
-            render json: json_response(JsonResponse::RESPONSE_STATUS[:ERROR],
-                                        "Tag not found.")
-            return
-        end 
-
-        @articles = @tag.articles
-        render json: json_response(JsonResponse::RESPONSE_STATUS[:SUCCESS],
-                                    @articles)
-    end
-
-    private
-        def tag_params
-            params.permit(:name)
-        end
-
-        def json_response(status, message="")
-            {
-                "status" => status,
-                "message" => message
-            }
-        end
 end
