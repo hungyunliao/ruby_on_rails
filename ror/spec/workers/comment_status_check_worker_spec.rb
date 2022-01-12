212697f29e7dd552b7866f4df7eb1e40f7c6edfd
Liao <hungyunl@88665a279eef.ant.amazon.com>
2022-01-11 16:21:24 -0800

    Implement spec comment_status_check_worker_spec.rb

--- ror/spec/workers/comment_status_check_worker_spec.rb
+++ ror/spec/workers/comment_status_check_worker_spec.rb
@@ -1,4 +1,38 @@
 require 'rails_helper'
+
 RSpec.describe CommentStatusCheckWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.perform' do
+    let(:article) do
+      @article = Article.new(title: 'article title', body: 'article body', status: 'public')
+      @article.save
+      @article
+    end
+
+    context 'with a body that contains bad words' do
+      let(:comment) do
+        @comment = article.comments.create(body: 'comment body worse', commenter: 'comment commenter')
+        @comment.save
+
+        CommentStatusCheckWorker.new.perform(@comment.id)
+
+        @comment = Comment.find(@comment.id)
+      end
+
+
+      it('flags the comment') { expect(comment.submit_status).to eq('flagged') }
+    end
+
+    context 'with a body that does not contain bad words' do
+      let(:comment) do
+        @comment = article.comments.create(body: 'comment body good', commenter: 'comment commenter')
+        @comment.save
+
+        CommentStatusCheckWorker.new.perform(@comment.id)
+
+        @comment = Comment.find(@comment.id)
+      end
+
+      it('approves the comment') { expect(comment.submit_status).to eq('approved') }
+    end
+  end
 end
