fec772743880965b60ccbd8b73339cedcf5c39cb
Liao <hungyunl@88665a279eef.ant.amazon.com>
2022-01-12 11:43:59 -0800

    Update tags_request_spec.rb corresponding to AMS change in Tag Controller.

--- ror/spec/requests/tags_request_spec.rb
+++ ror/spec/requests/tags_request_spec.rb
@@ -10,31 +10,31 @@ RSpec.describe 'Tag requests', type: :request do
   @existed_tag_id      = nil
 
   before do
-    post '/tags', params: { :name => existed_tag_name }
+    post '/tags', params: { 'tag[name]' => existed_tag_name }
 
-    @existed_tag_object = JSON.parse(response.body)['message']
+    @existed_tag_object = JSON.parse(response.body)['tag']
     @existed_tag_id     = @existed_tag_object['id']
 
     get '/tags'
 
-    @existed_tags_length = JSON.parse(response.body)['message'].size
+    @existed_tags_length = JSON.parse(response.body)['tags'].size
   end
 
   describe 'POST /tags' do
     context 'with a tag name not yet exist' do
       let(:new_tag_name) { 'newTagName' }
 
-      before { post '/tags', params: { :name => new_tag_name } }
+      before { post '/tags', params: { 'tag[name]' => new_tag_name } }
 
-      it('creates a new tag with the tag name') { expect(JSON.parse(response.body)['message']['name']).to eq(new_tag_name) }
+      it('creates a new tag with the tag name') { expect(JSON.parse(response.body)['tag']['name']).to eq(new_tag_name) }
     end
 
     context 'with a tag name already existed' do
       let(:expected) { { 'name' => ['has already been taken'] } }
 
-      before { post '/tags', params: { :name => existed_tag_name } }
+      before { post '/tags', params: { 'tag[name]' => existed_tag_name } }
 
-      it('returns an error message') { expect(JSON.parse(response.body)['message']).to include(expected) }
+      it('returns an error message') { expect(JSON.parse(response.body)['errors']).to include(expected) }
     end
   end
 
@@ -42,17 +42,17 @@ RSpec.describe 'Tag requests', type: :request do
     let(:updated_tag_name) { 'updatedTagName' }
 
     context 'with a valid tag id' do
-      before { put "/tags/#{@existed_tag_id}", params: { :name => updated_tag_name } }
+      before { put "/tags/#{@existed_tag_id}", params: { 'tag[name]' => updated_tag_name } }
 
-      it('updates the name of the tag') { expect(JSON.parse(response.body)['message']['name']).to eq(updated_tag_name) }
+      it('updates the name of the tag') { expect(JSON.parse(response.body)['tag']['name']).to eq(updated_tag_name) }
     end
 
     context 'with an invalid tag id' do
-      let(:expected) { { 'errors' => { 'id' => ['errors.messages.not_found'] } } }
+      let(:expected) { {'id' => ['errors.messages.not_found']} }
 
       before { put "/tags/#{invalid_tag_id}", params: { :name => updated_tag_name } }
 
-      it('returns an error message') { expect(JSON.parse(response.body)).to eq(expected) }
+      it('returns an error message') { expect(JSON.parse(response.body)['errors']).to eq(expected) }
     end
   end
 
@@ -64,21 +64,21 @@ RSpec.describe 'Tag requests', type: :request do
       context 'with a filter that matches the tag name' do
         before { get "/tags?filter=#{right_filter_query}" }
 
-        it('returns tags containing the tag') { expect(JSON.parse(response.body)['message']).to include(@existed_tag_object) }
+        it('returns tags containing the tag') { expect(JSON.parse(response.body)['tags']).to include(@existed_tag_object) }
       end
 
       context 'with a filter that does not match any tag names' do
         before { get "/tags?filter=#{wrong_filter_query}" }
 
-        it('returns tags not containing the tag') { expect(JSON.parse(response.body)['message']).not_to include(@existed_tag_object) }
+        it('returns tags not containing the tag') { expect(JSON.parse(response.body)['tags']).not_to include(@existed_tag_object) }
       end
     end
 
     context 'without the filter query param' do
       before { get '/tags' }
 
-      it('has the length equals to the target length') { expect(JSON.parse(response.body)['message'].size).to eq(@existed_tags_length) }
-      it('returns tags containing the tag')            { expect(JSON.parse(response.body)['message']).to include(@existed_tag_object) }
+      it('has the length equals to the target length') { expect(JSON.parse(response.body)['tags'].size).to eq(@existed_tags_length) }
+      it('returns tags containing the tag')            { expect(JSON.parse(response.body)['tags']).to include(@existed_tag_object) }
     end
   end
 
@@ -109,11 +109,15 @@ RSpec.describe 'Tag requests', type: :request do
 
       before do
         post "/articles/#{@existed_article_id}/tags", params: { :tag_id => @existed_tag_id }
-        @returned_tag = JSON.parse(response.body)['message'][0]
+        @returned_tag = JSON.parse(response.body)['tags'][0]
       end
 
-      it('attaches the tag to the article')              { expect(@returned_tag).to include({'name' => existed_tag_name}) }
-      it('increases the tagging count by 1 for the tag') { expect(@returned_tag['taggings_count']).to eq(1) }
+      it('attaches the tag to the article') { expect(@returned_tag).to include({'name' => existed_tag_name}) }
+
+      it 'increases the tagging count by 1 for the tag' do
+        @tag = Tag.find_by(id: @existed_tag_id)
+        expect(@tag.taggings_count).to eq(1)
+      end
     end
   end
 
@@ -125,7 +129,7 @@ RSpec.describe 'Tag requests', type: :request do
 
       before do
         delete "/articles/#{@existed_article_id}/tags/#{@existed_tag_id}"
-        @returned_tag = JSON.parse(response.body)['message']
+        @returned_tag = JSON.parse(response.body)['tags']
       end
 
       it('detaches the tag from the article') { expect(@target_tag).to eq(nil) }
@@ -143,12 +147,12 @@ RSpec.describe 'Tag requests', type: :request do
 
       it 'deletes the tag' do
         get '/tags'
-        expect(JSON.parse(response.body)['message']).not_to include(@existed_tag_object)
+        expect(JSON.parse(response.body)['tags']).not_to include(@existed_tag_object)
       end
 
       it 'detaches the tag from the article' do
         get "/articles/#{@existed_article_id}/tags"
-        expect(JSON.parse(response.body)['message']).not_to include(@existed_tag_object)
+        expect(JSON.parse(response.body)['tags']).not_to include(@existed_tag_object)
       end
     end
 
@@ -160,12 +164,12 @@ RSpec.describe 'Tag requests', type: :request do
 
       it 'returns an error' do
         @expected = { 'base' => ['Cannot delete Tag. Tag being referenced.'] }
-        expect(JSON.parse(response.body)['message']).to eq(@expected)
+        expect(JSON.parse(response.body)['errors']).to eq(@expected)
       end
 
       it 'does not detach the tag from the article' do
         get "/articles/#{@existed_article_id}/tags"
-        expect(JSON.parse(response.body)['message'][0]).to include({'name' => existed_tag_name})
+        expect(JSON.parse(response.body)['tags'][0]).to include({'name' => existed_tag_name})
       end
     end
   end
